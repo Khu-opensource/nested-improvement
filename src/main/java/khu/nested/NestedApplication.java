0f77978841a33a780030e36014f3f36d36be19ba
yxhwxn <officeyh9819@gmail.com>
2024-05-06 20:00:26 +0900

    :rocket: Init: project create

---
+++ src/main/java/khu/nested/NestedApplication.java
@@ -0,0 +1,13 @@
+package khu.nested;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/khu/nested/NestedApplicationTests.java
@@ -0,0 +1,13 @@
+package khu.nested;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
